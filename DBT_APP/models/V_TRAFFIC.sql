ab0a3a3e7a41a63614733386674913536626f61c
David Turcotte <david.turcotte@appgroup.ca>
2023-02-22 14:27:56 -0500

    change date formyla

--- DBT_APP/models/V_TRAFFIC.sql
+++ DBT_APP/models/V_TRAFFIC.sql
@@ -4,10 +4,10 @@
 select 
     column_0 as store_id,
     sum(column_2) as traffic,
-    CAST(column_1 AS DATE) date
+    MAX(LEFT(column_1,10)) as [date]
 from
     {{ source('sftp', 'smstraffic') }}
-group by 1
+group by column_0,column_1
 
 UNION 
 
