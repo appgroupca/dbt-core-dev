5c3fc70456d499a1c5645ceed78a9af97032ef52
David Turcotte <david.turcotte@appgroup.ca>
2023-02-24 15:12:25 -0500

    commit V_TRAFFIC

--- DBT_APP/models/V_TRAFFIC.sql
+++ DBT_APP/models/V_TRAFFIC.sql
@@ -4,8 +4,19 @@
 select 
     column_0 as store_id,
     sum(column_2) as traffic,
-    MAX(LEFT(column_1,10)) as [date]
+    LEFT(column_1,10) as [date]
 from
     {{ source('sftp', 'smstraffic') }}
-group by column_0
+group by column_0,LEFT(column_1,10)
+
+UNION ALL
+
+select
+    store_id,
+    traffic,
+    CONVERT(VARCHAR,[Date],23) as [date]
+from 
+    {{ source('sftp', 'smstraffic_historical') }}
+WHERE
+    CONVERT(VARCHAR,[Date],23) < '2023-01-09'
 
