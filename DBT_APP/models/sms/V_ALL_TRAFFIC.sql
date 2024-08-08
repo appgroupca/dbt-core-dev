83d6b44cfa1147c97992d315c1163654baac8ac9
David Turcotte <david.turcotte@appgroup.ca>
2024-08-08 09:56:50 -0400

    delete old tables

--- DBT_APP/models/sms/V_ALL_TRAFFIC.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='view') }}
-
-select * from {{ ref("V_TRAFFIC") }}
-UNION ALL
-select * from {{ ref("V_HIST_TRAFFIC") }}
--- DBT_APP/models/sms/V_HIST_TRAFFIC.sql
+++
@@ -1,8 +0,0 @@
-select
-    store_id,
-    traffic,
-    CONVERT(VARCHAR,[Date],23) as [date]
-from 
-    {{ source('sms', 'storetraffic_historical') }}
-WHERE
-    CONVERT(VARCHAR,[Date],23) < '2023-01-09'
