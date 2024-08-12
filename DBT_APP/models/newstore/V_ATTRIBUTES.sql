2da672eac6aacfcfbddb2ded84b7981b5633deec
David Turcotte <david.turcotte@appgroup.ca>
2024-08-12 14:42:49 -0400

    commit added country in V_ATTRIBUTES

--- DBT_APP/models/newstore/V_ATTRIBUTES.sql
+++ DBT_APP/models/newstore/V_ATTRIBUTES.sql
@@ -45,5 +45,5 @@ AND REVISION.REVISION = ISNULL(JSON_VALUE(body,'$.payload.revision'),'')
 ) AS SOURCE_TABLE
 
 PIVOT (
-  MAX([VALUE]) FOR [NAME] IN ([birthdayMonth],[gdpr_accepted],[clientType],[gender],[language])
+  MAX([VALUE]) FOR [NAME] IN ([birthdayMonth],[gdpr_accepted],[clientType],[gender],[language],[country])
 ) AS PIVOT_TABLE
