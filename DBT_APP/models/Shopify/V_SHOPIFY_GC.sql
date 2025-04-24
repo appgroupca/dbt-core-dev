4b691935d389ac2c3b8d6978d73975f8c26afc73
David Turcotte <david.turcotte@appgroup.ca>
2025-04-24 09:18:00 -0400

    added main view changes

--- DBT_APP/models/Shopify/V_SHOPIFY_GC.sql
+++ DBT_APP/models/Shopify/V_SHOPIFY_GC.sql
@@ -2,32 +2,32 @@
 
 WITH CA_GC
 as (
-		SELECT * from {{ref("V_SHOPIFY_GC_CA")}}
+		SELECT * from {{ref("T_SHOPIFY_GC_CA")}}
  	),
 
 US_GC
 as (
-	SELECT * from {{ref("V_SHOPIFY_GC_US")}}
+	SELECT * from {{ref("T_SHOPIFY_GC_US")}}
 ),
 
 EU_GC
 as (
-	SELECT * from {{ref("V_SHOPIFY_GC_EU")}}
+	SELECT * from {{ref("T_SHOPIFY_GC_EU")}}
 ),
 
 UK_GC
 as (
-	SELECT * from {{ref("V_SHOPIFY_GC_UK")}}
+	SELECT * from {{ref("T_SHOPIFY_GC_UK")}}
 ),
 
 SKCA_GC
 as (
-	SELECT * from {{ref("V_SHOPIFY_GC_SK_CA")}}
+	SELECT * from {{ref("T_SHOPIFY_GC_SK_CA")}}
 ),
 
 SKUS_GC
 as (
-	SELECT * from {{ref("V_SHOPIFY_GC_SK_US")}}
+	SELECT * from {{ref("T_SHOPIFY_GC_SK_US")}}
 )
 
 select * from CA_GC
