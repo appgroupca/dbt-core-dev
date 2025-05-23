e674b499864cd71517e51f61ee890a72b85a07f7
David Turcotte <david.turcotte@appgroup.ca>
2025-05-23 15:54:12 -0400

    commmit

--- DBT_APP/models/centricplm/T_STYLE_FEATURES.sql
+++ DBT_APP/models/centricplm/T_STYLE_FEATURES.sql
@@ -35,4 +35,4 @@ FROM CC1 c1
     LEFT JOIN CN1 c2 on c1.id=c2.id and c1.color_code_rank=c2.color_name_rank
     LEFT JOIN CA1 c7 on c1.id=c7.id and c1.color_code_rank=c7.color_active_rank
 
-WHERE c7.color_active='true' and c1.development_type='REGULAR'
+WHERE c7.color_active='true' and c1.development_type='REGULAR' and c1.active_style='1'
